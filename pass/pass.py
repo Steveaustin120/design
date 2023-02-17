e2687ccb700ba82937b53088070cc4aec0eb0d98
ByteBrewer <odhiambostephen947@gmail.com>
2023-02-17 18:03:17 +0300

    Add files via upload

---
+++ pass/pass.py
@@ -0,0 +1,11 @@
+import random
+
+lower_case = "abcdefghijklmnopqrstuvwxyz"
+upper_case = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+number = "0123456789"
+symbols = "@#$%&!?/\"
+use_for = lower_case + upper_case + number + symbols
+length_for_pass = 8
+
+password = "".join(random.simple(Use))
+print("Your generated password is:")
